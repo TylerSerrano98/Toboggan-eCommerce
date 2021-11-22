9592cd49c555978a077556dde727800ca6ece8c4
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-11-22 11:07:43 -0600

    added the model files we will need based off erd, draft code for ShoppingCart.cs complete.

---
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerceStarterCode.Models
+{
+    public class ShoppingCart
+    {
+        [Key] 
+        public string Id { get; set; }
+        
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
